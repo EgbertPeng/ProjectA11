d4626acddb199a0715032e6a10af22215aafe2fa
EgbertPeng <1273483319@qq.com>
2019-02-22 10:09:39 +0800

    添加了活动信息界面

---
+++ pages/activity/activity.js
@@ -0,0 +1,97 @@
+// pages/activity/activity.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      articles:[
+      {
+          date:'01月12日',
+          details:[
+              {
+                imgUrl:'/images/swiper/01.jpg',
+                title: '星创百万读书会（线上微信群）分享《富爸...',
+                info: '1月12日上午，中国共产党杭州电子科技大学第三次代表大会在选举产生新一届党委、纪委，通过关于党委工作报告的决议和关于对于...'
+              },
+              {
+                imgUrl: '/images/swiper/02.jpg',
+                title:'音乐节潮流穿搭公式',
+                info:'进入四月，国内各大音乐节就近在眼前了。除了跟着音乐嗨翻全场，各路美女帅哥也纷纷祭出各自穿搭大招。跟日常搭配相比，音乐..'
+             }
+          ]
+      },
+      {
+        date:'01月13日',
+        details:[
+          {
+            imgUrl: '/images/swiper/03.jpg',
+            title:'星创百万读书会（线上微信群）分享《富爸...',
+            info: '1月12日上午，中国共产党杭州电子科技大学第三次代表大会在选举产生新一届党委、纪委，通过关于党委工作报告的决议和关于对于...'
+          },
+          {
+            imgUrl: '/images/swiper/04.jpg',
+            title: '音乐节潮流穿搭公式',
+            info: '进入四月，国内各大音乐节就近在眼前了。除了跟着音乐嗨翻全场，各路美女帅哥也纷纷祭出各自穿搭大招。跟日常搭配相比，音乐..'
+          }
+        ]
+      }
+      ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
