4316ab91c4e9f6c29acac51d5509e2c352126a81
EgbertPeng <1273483319@qq.com>
2019-02-27 18:21:08 +0800

    私信和通知界面已完成

--- pages/notification/notification.js
+++ pages/notification/notification.js
@@ -32,6 +32,54 @@ Page({
         nfcInfo: '赞了你',
         nfcDate: '2018/11/22'
       },
+    ],
+
+    articles: [
+      {
+        date: '01月12日',
+        details: [
+          {
+            imgUrl: '/images/swiper/01.jpg',
+            title: '星创百万读书会（线上微信群）分享《富爸...',
+            info: '1月12日上午，中国共产党杭州电子科技大学第三次代表大会在选举产生新一届党委、纪委，通过关于党委工作报告的决议和关于对于...'
+          },
+          {
+            imgUrl: '/images/swiper/02.jpg',
+            title: '音乐节潮流穿搭公式',
+            info: '进入四月，国内各大音乐节就近在眼前了。除了跟着音乐嗨翻全场，各路美女帅哥也纷纷祭出各自穿搭大招。跟日常搭配相比，音乐..'
+          }
+        ]
+      },
+      {
+        date: '01月13日',
+        details: [
+          {
+            imgUrl: '/images/swiper/03.jpg',
+            title: '星创百万读书会（线上微信群）分享《富爸...',
+            info: '1月12日上午，中国共产党杭州电子科技大学第三次代表大会在选举产生新一届党委、纪委，通过关于党委工作报告的决议和关于对于党组织来说'
+          },
+          {
+            imgUrl: '/images/swiper/04.jpg',
+            title: '音乐节潮流穿搭公式',
+            info: '进入四月，国内各大音乐节就近在眼前了。除了跟着音乐嗨翻全场，各路美女帅哥也纷纷祭出各自穿搭大招。跟日常搭配相比，音乐..'
+          }
+        ]
+      },
+      {
+        date: '01月14日',
+        details: [
+          {
+            imgUrl: '/images/swiper/03.jpg',
+            title: '星创百万读书会（线上微信群）分享《富爸...',
+            info: '1月12日上午，中国共产党杭州电子科技大学第三次代表大会在选举产生新一届党委、纪委，通过关于党委工作报告的决议和关于对于党组织来说'
+          },
+          {
+            imgUrl: '/images/swiper/04.jpg',
+            title: '音乐节潮流穿搭公式',
+            info: '进入四月，国内各大音乐节就近在眼前了。除了跟着音乐嗨翻全场，各路美女帅哥也纷纷祭出各自穿搭大招。跟日常搭配相比，音乐..'
+          }
+        ]
+      }
     ]
   },
   
@@ -43,7 +91,7 @@ Page({
         console.log(res.screenHeight)//手机屏幕高度
         that.setData({
           winWidth: res.windowWidth,
-          winHeight: res.windowHeight
+          winHeight: res.windowHeight-44    //44为tabbar高度
         });
       },
    
