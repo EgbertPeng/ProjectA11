2b5597ec8fe6f985b5abc89f3117d7106c44461c
EgbertPeng <1273483319@qq.com>
2019-03-22 19:40:20 +0800

    老师推荐界面已上传

---
+++ pages/recTea/recTea.js
@@ -0,0 +1,95 @@
+// pages/recTea/recTea.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    teas:[{
+      teaImg:'/images/teacher/tea1.png',
+      name:'egbert',
+      school:'杭州电子科技大学',
+      classNum: 23,
+      classLearned: 245
+    },
+    {
+      teaImg: '/images/teacher/tea2.png',
+      name: 'nick',
+      school: '清华大学',
+      classNum: 23,
+      classLearned: 245
+    },
+    {
+      teaImg: '/images/teacher/tea3.png',
+     name:'tom',
+      school:'南开大学',
+      classNum: 23,
+      classLearned: 245
+    },
+    {
+      teaImg: '/images/teacher/tea4.png',
+      name: 'john',
+     school: '浙江大学',
+      classNum: 23,
+        classLearned: 245
+    },
+    
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
